a0a233636f7483f7512421ff393b1b68f91fe841
Edgar F Herrera <132973538+EdgarHerrera2023@users.noreply.github.com>
2023-07-07 02:03:33 -0300

    Add files via upload
    Enunciado del programa a desarrollar:
    Realizar un programa en Python que conste de la conversión de °C a °F, °K.
    El mismo debe permitirle al usuario el ingreso del número a convertir en el tipo de dato que se considere correspondiente y con un mensaje que justifique cada conversión mostrándolo por pantalla.
    Pueden consultar en internet las equivalencias para la conversión a realizar
    Tener en cuenta que la calificación para la aprobación es mayor o igual a 6(seis). 
    Los nombres de las variables y de las funciones a cargar quedan a criterio del estudiante que está en examen.

---
+++ Examen EdgarHerrera.py
@@ -0,0 +1,9 @@
+def conversion_centigrados():
+    centigrados = float(input("Ingrese la temperatura en grados Centigrados"))
+
+    
+    # Conversion a grados Kelvin 
+    Kelvin = centigrados + 273.15
+    print("La temperatura en grados Kelvin es:", Kelvin)
+    
+conversion_centigrados()    
