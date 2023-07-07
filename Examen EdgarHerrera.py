30f39f616dda1e08442c17a7a7a67aa45a479db6
Edgar F Herrera <132973538+EdgarHerrera2023@users.noreply.github.com>
2023-07-07 02:30:48 -0300

    Update Examen EdgarHerrera.py
    corregido lo que faltaba

--- Examen EdgarHerrera.py
+++ Examen EdgarHerrera.py
@@ -1,7 +1,10 @@
 def conversion_centigrados():
     centigrados = float(input("Ingrese la temperatura en grados Centigrados"))
 
-    
+    # Conversion a grados Fahrenheit
+    fahrenheit = centigrados * 9/5 + 32
+    print("La temperatura en grados Fahrenheit es:", fahrenheit) 
+   
     # Conversion a grados Kelvin 
     Kelvin = centigrados + 273.15
     print("La temperatura en grados Kelvin es:", Kelvin)
